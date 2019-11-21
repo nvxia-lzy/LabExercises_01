d0f7768f3b61ddef58aee7316e07a0205346e010
nvxia-lzy <56499473+nvxia-lzy@users.noreply.github.com>
2019-11-21 14:36:28 +0800

    Create SimpleCalculator.h

---
+++ 201816040227/Lab6/SimpleCalculator.h
@@ -0,0 +1,11 @@
+// Lab Exercise 6: SimpleCalculator.h
+
+// class SimpleCalculator definition
+class SimpleCalculator
+{
+public: 
+   double add( double, double ) const;/* Write prototype for add member function */
+   double subtract( double, double ) const; 
+   double multiply( double, double ) const;
+   double divide( double, double ) const;/* Write prototype for divide member function */
+}; // end class SimpleCalculator
